bf05b9c8d61cce2e22100ad0fab06de70d1e5d7f
djande18 <132233922+djande18@users.noreply.github.com>
2023-10-24 14:56:35 -0700

    initial commit

--- sarcastic.py
+++ sarcastic.py
@@ -1,4 +1,19 @@
 import random
+import tkinter as tk
+
+#GUI
+
+window = tk.Tk()
+window.title("Sarcasm Generator")
+
+text_box = tk.Entry(window)
+text_box.pack(pady=10)
+
+text_box.pack(anchor='center')
+
+window.mainloop()
+
+#Scripting
 
 def sarcasm(text):
     text = text.lower()
@@ -14,4 +29,5 @@ def sarcasm(text):
 
 text = input("What would you like to capitalize?: \n")
 
-print(sarcasm(text))
+output = sarcasm(text)
+
