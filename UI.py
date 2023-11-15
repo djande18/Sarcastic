90b7a7e1d83213976485b37ef062d4c2a0685f41
djande18 <dylananderson123@gmail.com>
2023-11-15 12:46:25 -0700

    Working on UI

--- UI.py
+++ UI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import PhotoImage
 import sarcasm
 import pyperclip
 
@@ -7,8 +8,15 @@ answer = ""
 
 def on_click():
     global answer
+
+    if button.cget("image") == clicked:
+        button.config(image=unclicked)
+    else:
+        button.config(image=clicked)
+
     text = text_field.get()
-    answer += sarcasm.sarcasm(text)
+    answer = sarcasm.sarcasm(text)
+
 
     result.delete(1.0, tk.END)
     result.insert(tk.END, answer)
@@ -22,8 +30,8 @@ def copy():
 
 window = tk.Tk()
 window.title("Testing")
-window.geometry("688x203")
-window.resizable(False,False)
+
+
 
 label = tk.Label(window, text="Type a word or sentence:")
 label.grid(row=0, column=0,padx=10,pady=10,sticky="w")
@@ -31,13 +39,16 @@ label.grid(row=0, column=0,padx=10,pady=10,sticky="w")
 text_field = tk.Entry(window,width=75)
 text_field.grid(row=1, column=0, padx=10, pady=0)
 
-button = tk.Button(window, text="Generate Sarcasm",command=on_click)
+clicked = PhotoImage(file = "ButtonPics\Property 1=Variant2.png")
+unclicked = PhotoImage(file = "ButtonPics\Property 1=Default.png")
+
+button = tk.Button(window, image=clicked,command=on_click,borderwidth=0, relief=tk.FLAT)
 button.grid(row=1, column=2, padx=30, pady=0)
 
 result = tk.Text(window, height=5, width=70)
 result.grid(row = 2,column=0,padx=10,pady=10,sticky="w")
 
 copy_button = tk.Button(window, text="Copy to Clipboard",command=copy)
-copy_button.grid(row=3, column=0, padx=10, pady=0,sticky="w")
+copy_button.grid(row=3, column=0, padx=10, pady=10,sticky="w")
 
 window.mainloop()
